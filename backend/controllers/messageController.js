30a93ca320a37e732901817d20b9acba44c8be30
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-10 08:49:28 -0700

    Controller to handle messaging between users

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,26 @@
+// messageController.js
+const Message = require('../models/Message');
+
+const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const createMessage = async (req, res) => {
+  const message = new Message(req.body);
+  try {
+    const newMessage = await message.save();
+    res.status(201).json(newMessage);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getMessages,
+  createMessage,
+};
