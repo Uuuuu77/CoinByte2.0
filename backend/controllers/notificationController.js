4dcab1a0c3ea8adb53ba717548d4b128265f9553
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-10 08:46:25 -0700

    Controller to manage user notifications

---
+++ backend/controllers/notificationController.js
@@ -0,0 +1,26 @@
+// notificationController.js
+const Notification = require('../models/Notification');
+
+const getNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find();
+    res.json(notifications);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const createNotification = async (req, res) => {
+  const notification = new Notification(req.body);
+  try {
+    const newNotification = await notification.save();
+    res.status(201).json(newNotification);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getNotifications,
+  createNotification,
+};
