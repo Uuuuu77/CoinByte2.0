a2778a7b5c97ee37b5bcf87a40ed91eb9b2d7f60
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-10 02:30:57 -0700

    Mongodb configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,4 @@
+// db.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -5,11 +6,15 @@ dotenv.config();
 
 const dbURI = process.env.MONGO_URI;
 
+// Connect to MongoDB
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+  .catch((err) => {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(1); // Terminate process if database connection fails
+  });
 
 module.exports = mongoose;
