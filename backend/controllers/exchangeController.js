422dc0cae71cd64e8244e09c17f109b010f55b38
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-10 02:45:46 -0700

    Controls the exchange transcation

--- backend/controllers/exchangeController.js
+++ backend/controllers/exchangeController.js
@@ -1,33 +1,44 @@
+// exchangeController.js
 const Transaction = require('../models/Transaction');
 const User = require('../models/User');
 const { sendEmail } = require('../utils/sendEmail');
 
+// Create a new transaction
 const createTransaction = async (req, res) => {
-  const { type, amount, cryptocurrency } = req.body;
-  const userId = req.user._id;
+  try {
+    const { type, amount, cryptocurrency } = req.body;
+    const userId = req.user._id;
 
-  const transaction = new Transaction({
-    userId,
-    type,
-    amount,
-    cryptocurrency,
-  });
+    const transaction = new Transaction({
+      userId,
+      type,
+      amount,
+      cryptocurrency,
+    });
 
-  await transaction.save();
+    await transaction.save();
 
-  const user = await User.findById(userId);
+    const user = await User.findById(userId);
 
-  // Send notification email
-  sendEmail(user.email, 'New Transaction', `A new ${type} transaction of ${amount} ${cryptocurrency} has been made.`);
-
-  res.status(201).json({ message: 'Transaction successful', transaction });
+    sendEmail(user.email, 'New Transaction', `A new ${type} transaction of ${amount} ${cryptocurrency} has been made.`);
+    res.status(201).json({ message: 'Transaction successful', transaction });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
+// Get user transactions
 const getTransactions = async (req, res) => {
-  const userId = req.user._id;
-  const transactions = await Transaction.find({ userId });
+  try {
+    const userId = req.user._id;
+    const transactions = await Transaction.find({ userId });
 
-  res.json(transactions);
+    res.json(transactions);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
 module.exports = {
