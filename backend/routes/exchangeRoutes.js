d58b3c25d3d8e09d61705e4934745dbb9ee19b23
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-11 04:57:36 -0700

    Controls exchange transcation and routes

--- backend/routes/exchangeRoutes.js
+++ backend/routes/exchangeRoutes.js
@@ -1,10 +1,12 @@
+// exchangeRoutes.js
 const express = require('express');
 const exchangeController = require('../controllers/exchangeController');
+const authenticateToken = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/buy', exchangeController.buy);
-router.post('/sell', exchangeController.sell);
-router.get('/transactions', exchangeController.getTransactions);
+router.post('/buy', authenticateToken, exchangeController.buy);
+router.post('/sell', authenticateToken, exchangeController.sell);
+router.get('/transactions', authenticateToken, exchangeController.getTransactions);
 
 module.exports = router;
