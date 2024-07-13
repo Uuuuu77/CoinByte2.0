81e388fb1a92608a6401ca0276fab80b0327520d
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-13 10:36:59 -0700

    Updates

--- frontend/src/contexts/ThemeContext.jsx
+++ frontend/src/contexts/ThemeContext.jsx
@@ -4,24 +4,16 @@ import React, { createContext, useState, useEffect } from 'react';
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme || 'dark';
+  });
 
   // Function to toggle the theme
   const toggleTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark';
     setTheme(newTheme);
   };
-
-  // Load saved theme from local storage on initial load
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      setTheme(savedTheme);
-      document.body.className = savedTheme; // Apply saved theme class to body
-    } else {
-      document.body.className = theme;
-    }
-  }, [theme]);
 
   // Save theme preference to local storage and apply class to body
   useEffect(() => {
