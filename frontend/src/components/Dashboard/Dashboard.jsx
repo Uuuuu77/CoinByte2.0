758ef0e668f4a96ee11900d29cd425eecc1ff877
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-15 06:17:07 -0700

    Dashboard page

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+// Dashboard.jsx
 import React from 'react';
 import Portfolio from './Portfolio';
 import MarketData from './MarketData';
@@ -10,14 +11,31 @@ import './Dashboard.css';
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <div className="dashboard-content">
-        <PerformanceChart />
-        <div className="dashboard-widgets">
-          <Portfolio />
-          <MarketData />
-          <RecentTransactions />
-          <ActivityFeed />
-          <NewsFeed />
+      <div className="dashboard-header">
+        <h1>Dashboard</h1>
+      </div>
+      <div className="dashboard-widgets">
+        <div className="dashboard-left">
+          <div className="widget">
+            <Portfolio />
+          </div>
+          <div className="widget">
+            <PerformanceChart />
+          </div>
+          <div className="widget">
+            <RecentTransactions />
+          </div>
+        </div>
+        <div className="dashboard-right">
+          <div className="widget">
+            <MarketData />
+          </div>
+          <div className="widget">
+            <ActivityFeed />
+          </div>
+          <div className="widget">
+            <NewsFeed />
+          </div>
         </div>
       </div>
     </div>
