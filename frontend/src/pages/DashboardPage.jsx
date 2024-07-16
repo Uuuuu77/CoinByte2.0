2ba0526820c93ddad588368316a25c6288036f27
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-16 02:41:48 -0700

    Dashboard page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -12,30 +12,33 @@ import '../styles/DashboardPage.css';
 const DashboardPage = () => {
   return (
     <div className="dashboard">
-      <div className="dashboard-header">
-        <h1>Dashboard</h1>
-      </div>
-      <div className="dashboard-widgets">
-        <div className="dashboard-left">
-          <div className="widget">
-            <Portfolio />
-          </div>
-          <div className="widget">
-            <PerformanceChart />
-          </div>
-          <div className="widget">
-            <RecentTransactions />
-          </div>
+      <Sidebar /> {/* Include the Sidebar */}
+      <div className="dashboard-content">
+        <div className="dashboard-header">
+          <h1>Dashboard</h1>
         </div>
-        <div className="dashboard-right">
-          <div className="widget">
-            <MarketData />
+        <div className="dashboard-widgets">
+          <div className="dashboard-left">
+            <div className="widget">
+              <Portfolio />
+            </div>
+            <div className="widget">
+              <PerformanceChart />
+            </div>
+            <div className="widget">
+              <RecentTransactions />
+            </div>
           </div>
-          <div className="widget">
-            <ActivityFeed />
-          </div>
-          <div className="widget">
-            <NewsFeed />
+          <div className="dashboard-right">
+            <div className="widget">
+              <MarketData />
+            </div>
+            <div className="widget">
+              <ActivityFeed />
+            </div>
+            <div className="widget">
+              <NewsFeed />
+            </div>
           </div>
         </div>
       </div>
