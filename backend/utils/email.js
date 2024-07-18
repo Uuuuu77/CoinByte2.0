5dc2697f807af4b6bf3192836f05c169e31db332
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-18 07:28:27 -0700

    Email configuration

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,34 +1,18 @@
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
 
-// Create an OAuth2 client with the given credentials
-const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    'https://developers.google.com/oauthplayground'
-);
-
-oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
+// Configure the email transport using SMTP
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
 });
 
 async function sendEmail(subject, recipient, body) {
     try {
-        const accessToken = await oauth2Client.getAccessToken();
-
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                type: 'OAuth2',
-                user: process.env.EMAIL,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                refreshToken: process.env.REFRESH_TOKEN,
-                accessToken: accessToken.token
-            }
-        });
-
         const mailOptions = {
             from: process.env.EMAIL,
             to: recipient,
