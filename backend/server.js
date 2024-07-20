7ffc00d443c061d2146b433f09031a1a8625dbaf
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-20 02:27:10 -0700

    Start the application

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.use(errorHandler);
 
 // Start Server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
