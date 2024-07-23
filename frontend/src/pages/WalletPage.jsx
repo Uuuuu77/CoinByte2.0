43d4dc416885c3dda675a6221681ccb5465cad00
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-23 07:55:44 -0700

    Wallet page

--- frontend/src/pages/WalletPage.jsx
+++ frontend/src/pages/WalletPage.jsx
@@ -1,4 +1,4 @@
-// WalletPage.jsx
+// src/pages/WalletPage.jsx
 import React from 'react';
 import WalletOverview from '../components/Wallet/WalletOverview';
 import DepositForm from '../components/Wallet/DepositForm';
@@ -10,14 +10,26 @@ import '../styles/WalletPage.css';
 const WalletPage = () => {
   return (
     <div className="wallet-page">
-      <div className="wallet-section">
+      <div className="wallet-overview-section">
         <WalletOverview />
       </div>
-      <div className="wallet-section">
-        <DepositForm />
-        <WithdrawForm />
-        <TransferForm />
-	<ColdStorageForm />
+      <div className="wallet-forms-section">
+        <div className="wallet-row">
+          <div className="wallet-form">
+            <DepositForm />
+          </div>
+          <div className="wallet-form">
+            <WithdrawForm />
+          </div>
+        </div>
+        <div className="wallet-row">
+          <div className="wallet-form">
+            <TransferForm />
+          </div>
+          <div className="wallet-form">
+            <ColdStorageForm />
+          </div>
+        </div>
       </div>
     </div>
   );
