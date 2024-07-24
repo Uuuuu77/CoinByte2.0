d4e1116197c70f54778db99b9527ed76c0799f30
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-24 03:36:57 -0700

    Mockup data

---
+++ frontend/src/services/mock/commentsMock.js
@@ -0,0 +1,5 @@
+// commentsMock.js
+export const commentsMock = [
+  { id: 1, postId: 1, content: 'First comment', userId: 1 },
+  { id: 2, postId: 1, content: 'Second comment', userId: 2 }
+];
---
+++ frontend/src/services/mock/postsMock.js
@@ -0,0 +1,5 @@
+// postsMock.js
+export const postsMock = [
+  { id: 1, content: 'First post', userId: 1 },
+  { id: 2, content: 'Second post', userId: 2 }
+];
